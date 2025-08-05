75c606ba7c32432d155be586d692bb140c939048
LUFFY777549 <dragonsimagine1212@gmail.com>
2025-08-05 14:16:56 +0530

    Create Tagger/__init__.py

---
+++ Tagger/__init__.py
@@ -0,0 +1,12 @@
+from datetime import datetime
+start_time = datetime.utcnow()
+
+from pyrogram import Client
+from config import API_ID, API_HASH, BOT_TOKEN
+
+bot = Client(
+    "TAGGER",
+    api_id=API_ID,
+    api_hash=API_HASH,
+    bot_token=BOT_TOKEN,       
+)
